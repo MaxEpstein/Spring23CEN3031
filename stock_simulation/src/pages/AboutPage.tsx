fa603d37c3e4e83cfd5eb77fd56c993de9fe50bf
Jeanette Foreman <jeanette.foreman@ufl.edu>
2023-02-05 17:20:41 -0500

    Created contact form

--- stock_simulation/src/pages/AboutPage.tsx
+++ stock_simulation/src/pages/AboutPage.tsx
@@ -1,11 +1,11 @@
 export function AboutPage() {
     return  (
-        <div className={"contact"}>
-            <h1>About Us</h1>
+        <div className={"About"}>
+            <h1 style={{textAlign: "center"}}>About Us</h1>
             <p>
                 The purpose of this project is to allow users
                 to participate in the stock market risk free while
-                keeping up with the latest news and events. 
+                keeping up with the latest news and events.
             </p>
         </div>
 
