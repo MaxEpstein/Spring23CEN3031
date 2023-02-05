89a0d4e6047776fdb0133287d791175ab935c08c
Jeanette Foreman <jeanette.foreman@ufl.edu>
2023-02-05 15:51:20 -0500

    Update AboutPage.tsx

--- stock_simulation/src/pages/AboutPage.tsx
+++ stock_simulation/src/pages/AboutPage.tsx
@@ -1,3 +1,13 @@
 export function AboutPage() {
-    return <h1>About Page</h1>;
+    return  (
+        <div className={"contact"}>
+            <h1>About Us</h1>
+            <p>
+                The purpose of this project is to allow users
+                to participate in the stock market risk free while
+                keeping up with the latest news and events. 
+            </p>
+        </div>
+
+    );
   }
