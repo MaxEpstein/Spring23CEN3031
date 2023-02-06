8279678fffc7c873138f5b87d4e841a98b794e49
leoNoodle1121 <leandrosanchez@ufl.edu>
2023-02-06 18:05:23 -0500

    able to add and store current prices into a hashmap

--- src/main.go
+++ src/main.go
@@ -3,31 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
-
-	"github.com/leekchan/accounting"
 	"github.com/piquette/finance-go/quote"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	flag.Parse()
 
-	if len(flag.Args()) == 0 {
-		logrus.Fatalf("No argument provided, exected at least one stock symbol. Example: %v cldr goog aapl intc amd ...", os.Args[0])
+	var smbls []string
+	smbls = append(smbls, "aapl")
+	//smbls = append(smbls, "aapl")
+
+	stock_data := make(map[string][]uint)
+	sum := 1
+	for sum < 100 {
+		sum += 1
+		iter := quote.List(smbls) // This creates a list that unless
+		// you keep creating will return false and only run once
+		for iter.Next() {
+			q := iter.Quote()
+			stock_data[q.ShortName] = append(stock_data[q.ShortName], uint(q.Ask*100))
+
+		}
 	}
-
-	cf := accounting.Accounting{Symbol: "$", Precision: 2}
-	smbls := flag.Args()
-
-	iter := quote.List(smbls)
-
-	for iter.Next() {
-		q := iter.Quote()
-		fmt.Printf("------- %v -------\n", q.ShortName)
-		fmt.Printf("Current Price: %v\n", cf.FormatMoney(q.Ask))
-		fmt.Printf("52wk High: %v\n", cf.FormatMoney(q.FiftyTwoWeekHigh))
-		fmt.Printf("52wk Low: %v\n", cf.FormatMoney(q.FiftyTwoWeekLow))
-		fmt.Printf("-----------------\n")
-	}
+	fmt.Println(stock_data)
 }
