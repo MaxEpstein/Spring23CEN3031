3fcbb3682e36f3b5d2ad6823c975f27bf3bcc2f5
leoNoodle1121 <leandrosanchez@ufl.edu>
2023-02-07 11:04:11 -0500

    Main structures added

--- src/main.go
+++ src/main.go
@@ -1,29 +1,120 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	finance "github.com/piquette/finance-go"
+	chart "github.com/piquette/finance-go/chart"
+	"github.com/piquette/finance-go/datetime"
 	"github.com/piquette/finance-go/quote"
+	"time"
 )
 
-func main() {
-	flag.Parse()
+type stock struct {
+	symbol string
+	name   string
+	data   map[int64]uint
+	//@TODO any additional features needed add here
+}
 
-	var smbls []string
-	smbls = append(smbls, "aapl")
-	//smbls = append(smbls, "aapl")
+type data_list struct {
+	data map[string][]stock //For future scalability for etf's, stocks, crypto
+}
 
-	stock_data := make(map[string][]uint)
-	sum := 1
-	for sum < 100 {
-		sum += 1
-		iter := quote.List(smbls) // This creates a list that unless
-		// you keep creating will return false and only run once
-		for iter.Next() {
-			q := iter.Quote()
-			stock_data[q.ShortName] = append(stock_data[q.ShortName], uint(q.Ask*100))
+func add_historic_data(qt *finance.Quote, temp_stock *stock) {
+	//@TODO figure out pointer situaion and get maps to update accross
+	//@TODO add historic data  anythin behind 01/01/1970 UTC midnight is a negative int
+	p := &chart.Params{
+		Symbol: temp_stock.symbol,
+		Start:  &datetime.Datetime{Month: 1, Day: 1, Year: 1970},
+		End: &datetime.Datetime{Month: int(time.Now().Month()),
+			Day:  int(time.Now().Day()),
+			Year: int(time.Now().Year())},
+		Interval: datetime.OneDay, //@Todo might want to change this later
+	}
+	iter := chart.Get(p)
+
+	// Iterate over results. Will exit upon any error.
+	for iter.Next() {
+		b := iter.Bar()
+		fmt.Println(b)
+
+		// Meta-data for the iterator - (*finance.ChartMeta).
+		//fmt.Println(iter.Meta())
+	}
+
+	// Catch an error, if there was one.
+	//if iter.Err() != nil {
+	// Uh-oh!
+	//	panic(err)
+	//}
+	//It has something to do with
+	//func GetHistoricalQuote(symbol string, month int, day int, year int) (*finance.ChartBar, error)
+}
+
+func setup_main_working_list(s_type_name []string, s_type_sym []string) *data_list {
+	s_types := make(map[string][]string)
+
+	for i, item := range s_type_name {
+		s_types[item] = append(s_types[item], s_type_sym[i])
+	}
+
+	main_working_list := new(data_list)
+	main_working_list.data = make(map[string][]stock)
+
+	for st_type, symb_arr := range s_types {
+		for i, item := range symb_arr {
+			//here you can add more attributes
+
+			qt, err := quote.Get(item)
+			if err != nil {
+				panic(err)
+			}
+			//=========================
+			temp_stock := new(stock)
+			temp_stock.data = make(map[int64]uint)
+			temp_stock.symbol = item
+			temp_stock.name = qt.ShortName
+			add_historic_data(qt, temp_stock)
+			//@TODO any additional features needed add here
+			//https://piquette.io/projects/finance-go/ website for full list of things
+			//========================
+			//fmt.Println(temp_stock) // comment out when ready to continue
+			main_working_list.data[st_type] = append(main_working_list.data[st_type], *temp_stock)
+			i++ // to quite goLang
 
 		}
+
 	}
-	fmt.Println(stock_data)
+	return main_working_list
+}
+
+func update_data_list(working_list *data_list) {
+
+	for x, st_type := range working_list.data {
+
+		for i, st_symb1 := range st_type {
+			qt, err := quote.Get(st_symb1.symbol)
+			if err != nil {
+				panic(err)
+			}
+			st_symb1.data[time.Now().Unix()] = uint(qt.Ask * 100)
+			i++     //ignore
+			x += "" //ignore
+		}
+	}
+}
+
+func main() {
+
+	s_type_name := []string{"stock", "stock", "stock"}
+	s_type_sym := []string{"aapl", "aal", "intc"}
+	//Way to specify what type and symbol
+	main_working_list := setup_main_working_list(s_type_name, s_type_sym)
+
+	//wrap in some kind of time based loop
+	//for {
+	update_data_list(main_working_list)
+	fmt.Println(main_working_list)
+	//}
+
 }
