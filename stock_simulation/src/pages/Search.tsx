f46c5d4dba96ec42309ed0ed955e862144f55316
MaxEpstein <max.epstein@hotmail.com>
2023-02-15 00:26:15 -0500

    Formatted Search page. Accepts input and prints input to console. Changes to all caps

--- stock_simulation/src/pages/Search.tsx
+++ stock_simulation/src/pages/Search.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import { FormatCodeSettings } from 'typescript';
 
 export function Search() {
+    const [message, setMessage] = useState('');
+    const [updated, setUpdated] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement> ) => {
+    if (event.key === 'Enter') {
+      handleClick();
+    }
+};
+
+  const handleClick = () => {
+    setMessage("");
+    console.log(message.toUpperCase())
+  };
+
     return  (
         <div className="SearchTop">
-                <input type="Stock Ticker" placeholder="Stock Ticker"/>
-                <button className="submit" type="submit">Search</button>
+                <input type="text" placeholder="Stock Ticker" onChange={handleChange} value={message} name="message" id="message" onKeyDown={handleKeyDown}/>
+                <button className="submit" type="submit" onClick={handleClick}>Search</button>
         </div>
     );
-  }
+}
