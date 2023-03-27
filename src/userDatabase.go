43b0f65ce116dad48d5e973badd5013a0b9acfa1
Brayden Bevels <brayden.bevels@ufl.edu>
2023-03-27 13:56:27 -0400

    Functions to be written for user database

---
+++ src/userDatabase.go
@@ -0,0 +1,29 @@
+package main
+
+func addUser() {
+
+}
+
+func removeUser() {
+
+}
+
+func login() {
+
+}
+
+func logout() {
+
+}
+
+func addStockToWatchlist() {
+
+}
+
+func removeStockFromWatchlist() {
+
+}
+
+func getUser() {
+
+}
