dafaeed945d53e8971333ae615993020b66f261b
Brayden Bevels <brayden.bevels@ufl.edu>
2023-03-27 14:30:15 -0400

    more login progress

--- src/userDatabase.go
+++ src/userDatabase.go
@@ -1,14 +1,38 @@
 package main
 
+import (
+	"strings"
+)
+
+type user struct {
+	username        string
+	password        string
+	watchlistStocks []string
+}
+
+func initializeUserDatabase() {
+	userDatabase := make(map[string]user)
+	for k := range userDatabase {
+		delete(userDatabase, k)
+	}
+}
+
+func login(loginInfo string) {
+	loginInfo = strings.TrimSpace(loginInfo)            //get rid of leading and trailing spaces
+	loginInfoSeperated := strings.Split(loginInfo, ",") //replace with delimiter being used
+	username, password := loginInfoSeperated[0], loginInfoSeperated[1]
+	getAllUsers(username, password)
+}
+
+func getAllUsers(username string, password string) {
+
+}
+
 func addUser() {
 
 }
 
 func removeUser() {
-
-}
-
-func login() {
 
 }
 
@@ -21,9 +45,5 @@ func addStockToWatchlist() {
 }
 
 func removeStockFromWatchlist() {
-
-}
-
-func getUser() {
 
 }
