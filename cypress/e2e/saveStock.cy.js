dd7ce19856fecf00cead8e450a9e932bfbbcf34d
Jeanette Foreman <jeanette.foreman@ufl.edu>
2023-04-18 16:54:13 -0400

    saving stock cypress test
    saves stock to dashboard and then check if it appears

--- cypress/e2e/saveStock.cy.js
+++ cypress/e2e/saveStock.cy.js
@@ -1,5 +1,23 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/')
+    cy.contains('Search').click()
+    cy.url().should('include', '/search')
+
+    cy.get("input[type ='text']").type('AAPL')
+    cy.get('button').contains("Search").click()
+
+    cy.get('button').contains("Save to Dashboard").click()
+
+    cy.contains('Log In').click()
+    cy.url().should('include', '/LogIn')
+
+    cy.get("input[type ='text']").type('harry')
+    cy.get("input[type ='password']").type('password')
+
+    cy.contains("Log in").click()
+    cy.url().should('include', '/dashboard')
+
+    cy.contains('AAPL')
   })
 })
