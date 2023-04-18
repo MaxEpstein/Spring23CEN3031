fc3658957e2a8e11daa398bfdd224a7018fe06da
Jeanette Foreman <jeanette.foreman@ufl.edu>
2023-04-18 15:54:09 -0400

    restyled about page

--- stock_simulation/src/pages/AboutPage.tsx
+++ stock_simulation/src/pages/AboutPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import "./pageStyles.css";
 
 
@@ -20,26 +21,45 @@ export function AboutPage() {
             </div>
 
             <div className ="splitScreen">
-                <h1 style={{textAlign: "center", color: "midnightblue", alignItems: "center", paddingBottom: "50px",
-                }}> Meet the Makers!</h1>
-
-                <div className = "jeanetteInfo">
-                    <img src ={require("../images/ForemanAbout.jpg")} height = "300" width = "400"/>
-                    <p style={{textAlign: "center", font: "roboto", display: "inline"}}> Jeanette </p>
+                <h1
+                    style={{textAlign: "center", color: "midnightblue",
+                        alignItems: "center", paddingBottom: "50px",}}>
+                    Meet the Developers!
+                </h1>
+                <div className = "row">
+                    <div className = "columnL">
+                        <img src ={require("../images/ForemanAbout.jpg")} height = "300" width = "400"/>
+                    </div>
+                    <div className="columnR">
+                        <img src = {require("../images/IMG_3471.jpg")} height = "300" width = "400" />
+                    </div>
                 </div>
 
-                <div className = 'maxInfo'>
-                    <img src = {require("../images/IMG_3471.jpg")} height = "300" width = "400" />
-                    <p style={{textAlign: "center", font: "roboto"}}> Max </p>
+                <div className = "row">
+                    <div className = "columnL" style = {{marginLeft: 230}}>
+                        <h2>Jeanette</h2>
+                    </div>
+                    <div className = "columnR" style = {{marginLeft: 160}}>
+                        <h2>Max</h2>
+                    </div>
+                </div>
 
+                <div className = "row" style = {{marginTop: 10}}>
+                    <div className = "columnL">
+                        <img src = {require("../images/bkg3.jpg")}  height = "400" width = "400"/>
+                    </div>
+                    <div className={"columnR"}>
+                        <img src = {require("../images/bkg2.jpg")}  height = "400" width = "400"/>
+                    </div>
                 </div>
-                <div className = 'leandroInfo'>
-                    <img src = {require("../images/bkg3.jpg")}  height = "400" width = "400"/>
-                    <p style={{textAlign: "center", font: "roboto"}}> Leandro </p>
-                </div>
-                <div className = 'braydenInfo'>
-                    <img src = {require("../images/bkg2.jpg")}  height = "400" width = "400"/>
-                    <p style={{textAlign: "center", font: "roboto"}}> Brayden </p>
+
+                <div className = "row">
+                    <div className = "columnL" style = {{marginLeft: 230}} >
+                        <h2>Leandro</h2>
+                    </div>
+                    <div className = "columnR" style = {{marginLeft: 130}}>
+                        <h2>Brayden</h2>
+                    </div>
                 </div>
 
             </div>
