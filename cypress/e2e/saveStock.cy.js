d6655a2c174afb9bb4327a97cd2ddc9cc7fb0427
Jeanette Foreman <jeanette.foreman@ufl.edu>
2023-04-19 21:03:28 -0400

    Cypress tests

--- cypress/e2e/saveStock.cy.js
+++ cypress/e2e/saveStock.cy.js
@@ -1,16 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
-    cy.contains('Search').click()
-    cy.url().should('include', '/search')
-
-    cy.get("input[type ='text']").type('AAPL')
-    cy.get('button').contains("Search").click()
-
-    cy.get('button').contains("Save to Dashboard").click()
 
     cy.contains('Log In').click()
-    cy.url().should('include', '/LogIn')
+    cy.url().should('include', '/Login')
 
     cy.get("input[type ='text']").type('harry')
     cy.get("input[type ='password']").type('password')
@@ -18,6 +11,17 @@ describe('template spec', () => {
     cy.contains("Log in").click()
     cy.url().should('include', '/dashboard')
 
+    cy.contains('Search').click()
+    cy.url().should('include', '/search')
+
+    cy.get("input[type ='text']").type('AAPL')
+    cy.get('button').contains("Search").click()
+
+    cy.contains("Log In").click()
+
+    cy.contains('Dashboard').click()
+    cy.url().should('include', '/dashboard')
+    cy.wait(10000)
     cy.contains('AAPL')
   })
 })
--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,14 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/')
+
+    cy.contains('Log In').click()
+    cy.url().should('include', '/Login')
+
+    cy.get("input[type ='text']").type('harry')
+    cy.get("input[type ='password']").type('password')
+
+    cy.contains("Sign up").click()
+    cy.url().should('include', '/dashboard')
   })
 })
